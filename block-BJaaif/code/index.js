b60a70103560e37df6354b996460db587f7de695
Abhishek Kumar <abhivision3@gmail.com>
2021-02-02 10:28:14 +0530

    block5

--- block-BJaaif/code/index.js
+++ block-BJaaif/code/index.js
@@ -3,9 +3,19 @@
 ****************************************************************/
 
 /*** CHALLENGE 1 of 1 ***/
+// Create a function that accepts two inputs (name and age) and returns an object. Let's call this function `makePerson`. This function will:
+
+// 1. create an empty object
+// 2. add a `name` property to the newly created object with its value being the 'name' argument passed into the function
+// 3. add an `age` property to the newly created object with its value being the 'age' argument passed into the function
+// 4. return the object
 
 function makePerson(name, age) {
-  // add code here
+  let obj ={};
+  obj.name =name;
+  obj.age =age;
+
+  return obj;
 }
 
 var vicky = makePerson('Vicky', 24);
@@ -19,9 +29,13 @@ var vicky = makePerson('Vicky', 24);
 ****************************************************************/
 
 /*** CHALLENGE 1 of 3 ***/
+// Inside `personStore` object, create a property `greet` where the value is a function that logs "hello".
+
 
 var personStore = {
-  // add code here
+  greet: function(){
+    return `Hello`
+  }
 };
 
 // /********* Uncomment this line to test your work! *********/
@@ -29,8 +43,13 @@ var personStore = {
 
 /*** CHALLENGE 2 of 3 ***/
 
+
+// Create a function `personFromPersonStore` that takes as input a `name` and an `age`. When called, the function will create person objects using the `Object.create` method on the `personStore` object. And return the object with age and name.
 function personFromPersonStore(name, age) {
-  // add code here
+  let obj = Object.create(personStore);
+  obj.name =name;
+  obj.age =age;
+  return obj;
 }
 
 var sandra = personFromPersonStore('Sandra', 26);
@@ -42,7 +61,9 @@ var sandra = personFromPersonStore('Sandra', 26);
 
 /*** CHALLENGE 3 of 3 ***/
 
-// add code here
+personStore.introduce = function(){
+  console.log(`Hi, my name is ${this}`)
+}
 
 // sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
 
@@ -50,10 +71,15 @@ var sandra = personFromPersonStore('Sandra', 26);
                     USING THE 'NEW' KEYWORD
 ****************************************************************/
 
+
+// Create a function `PersonConstructor` that uses the `this` keyword to save a single property onto its scope called `greet`. `greet` should be a function that logs the string 'hello'.
+
 /*** CHALLENGE 1 of 3 ***/
 
 function PersonConstructor() {
-  // add code here
+  this.greet = function(){
+    console.log("hello");
+  }
 }
 
 // /********* Uncomment this line to test your work! *********/
@@ -62,11 +88,17 @@ var simon = new PersonConstructor();
 
 /*** CHALLENGE 2 of 3 ***/
 
-function personFromConstructor(name, age) {
-  // add code here
+// Create a function `personFromConstructor` that takes as input a `name` and an `age`. When called, the function will create person objects using the `new` keyword instead of the Object.create method.
+
+function PersonFromConstructor(name, age) {
+  this.name =name;
+  this.age =age;
+  this.greet = function(){
+    console.log("Hello")
+  }
 }
 
-var mike = personFromConstructor('Mike', 30);
+var mike = new PersonFromConstructor('Mike', 30);
 
 // /********* Uncomment these lines to test your work! *********/
 // console.log(mike.name); // -> Logs 'Mike'
@@ -85,20 +117,34 @@ var mike = personFromConstructor('Mike', 30);
 /*** CHALLENGE 1 of 3 ***/
 
 class PersonClass {
-  constructor() {
-    // add code here
+  constructor(name) {
+    this.name =name;
+    
   }
 
-  // add code here
+  greet(){
+    console.log("Hello")
+  }
 }
 
 // /********* Uncomment this line to test your work! *********/
 var george = new PersonClass();
 // george.greet(); // -> Logs 'hello'
 
+
+// Create a class `DeveloperClass` that creates objects by extending the `PersonClass` class. In addition to having a `name` property and `greet` method, `DeveloperClass` should have an `introduce` method. When called, `introduce` should log the string 'Hello World, my name is [name]'.
+
+
 /*** CHALLENGE 2 of 3 ***/
 
-// add code here
+class DeveloperClass extends PersonClass{
+  constructor(name){
+super(name);
+  }
+  introduce(){
+    console.log(`Hello , my name is ${this.name}`);
+  }
+}
 
 // /********* Uncomment these lines to test your work! *********/
 // var thai = new DeveloperClass('Thai', 32);
@@ -123,7 +169,9 @@ function userFactory(name, score) {
   return user;
 }
 
-var adminFunctionStore /* Put code here */;
+var adminFunctionStore = {};
+
+Object.setPrototypeOf(adminFunctionStore,userFunctionStore);
 
 function adminFactory(name, score) {
   // Put code here
@@ -136,3 +184,4 @@ var adminFromFactory = adminFactory('Eva', 5);
 // /********* Uncomment these lines to test your work! *********/
 // adminFromFactory.sayType() // -> Logs "I am a Admin"
 // adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
+// Create an object `adminFunctionStore` that has access to all methods in the `userFunctionStore` object, without copying them over individually.
